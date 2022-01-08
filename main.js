ec374f620673249044d16368be62863f8c4d3e92
Veer-29 <79202817+Veer-29@users.noreply.github.com>
2022-01-08 19:26:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function setup() {
   screen_width = window.innerWidth;
   screen_height = window.innerHeight;
   canvas = createCanvas(screen_width,screen_height-150);
-  canvas.position(0,200);
+  canvas.position(0,250);
 }
 
 function draw() {
